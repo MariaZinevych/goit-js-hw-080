54571643717ca613f7f25c8c703b55a785a02791
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-06-19 16:12:04 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+console.log(currentTime);
+
+player
+  .setCurrentTime(JSON.parse(currentTime))
+  .then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          'the time was less than 0 or greater than the video’s duration'
+        );
+
+        break;
+
+      default:
+        console.log('some other error occurred');
+        break;
+    }
+  });
