23a7b5837f772ae8d1d3d2f15e951534f419c361
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-06-19 15:14:54 +0200

    add task 1 and task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const makeGallery = createGalleryCards(galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', makeGallery);
+galleryContainer.style.listStyle = 'none';
+galleryContainer.addEventListener('click', onGalleryClick);
+
+function createGalleryCards(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `;
+    })
+    .join('');
+}
+
+function onGalleryClick(event) {
+  event.preventDefault();
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import trottle from 'lodash.throttle';
 
+const formVallue = document.querySelector('.feedback-form');
+
+initForm();
+
+formVallue.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(formVallue);
+  formData.forEach((value, name) => console.log(value, name));
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+});
+
+formVallue.addEventListener(
+  'input',
+  trottle(e => {
+    e.preventDefault();
+    let selectedFilter = localStorage.getItem('feedback-form-state');
+    selectedFilter = selectedFilter ? JSON.parse(selectedFilter) : {};
+    selectedFilter[e.target.name] = e.target.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(selectedFilter));
+  }, 500)
+);
+
+function initForm() {
+  let savedMessage = localStorage.getItem('feedback-form-state');
+  if (savedMessage) {
+    savedMessage = JSON.parse(savedMessage);
+    console.log(savedMessage);
+
+    Object.entries(savedMessage).forEach(([name, value]) => {
+      formVallue.elements[name].value = value;
+    });
+  }
+}
